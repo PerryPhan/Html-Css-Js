072cec52e133708bb669b9b57d47390cef120ed3
Phan Dai <phan.dai@globee.hk>
2022-02-22 15:54:50 +0700

    [FE] Done exercise 1, next exercise 2

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const app = express()
+const port = 5000
+const path = require('path');
+app.use(express.static(path.join(__dirname, 'public')));
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname + '/public/FE_exerceise_1.html'));
+})
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`)
+})
